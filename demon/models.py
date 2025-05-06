9c5166f31ee0335f6366dbd895c602c73cd455ea
antoin0 <antonespblc@gmail.com>
2025-05-06 18:17:00 +0200

    modified models.py to have skills (habilidades)

--- demon/models.py
+++ demon/models.py
@@ -29,7 +29,7 @@ class Personaje(models.Model):
     usuario = models.ForeignKey(UsuarioPersonalizado, on_delete=models.CASCADE, related_name="personajes")
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     clase = models.CharField(max_length=12, choices=CLASES,default='stalker')
-
+    habilidades = models.TextField(max_length=500,blank=True)
     #stats
     fuerza = models.IntegerField()
     velocidad = models.IntegerField()
